3a448fbaa46155f09e80850f90d50ed21fc0f339
suleesulee <56288518+suleesulee@users.noreply.github.com>
2021-09-03 19:12:45 +0900

    [userservice] 유저 서비스 첫 등록

---
+++ MAKDUCK-user-service/src/main/java/com/suleesulee/MAKDUCKuserservice/MakduckUserServiceApplication.java
@@ -0,0 +1,15 @@
+package com.suleesulee.MAKDUCKuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MakduckUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakduckUserServiceApplication.class, args);
+	}
+
+}
