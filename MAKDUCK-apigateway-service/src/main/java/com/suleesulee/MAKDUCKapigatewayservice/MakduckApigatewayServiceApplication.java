9181f08b3b1bea878e2c3df9e17ef8eeec91291c
suleesulee <56288518+suleesulee@users.noreply.github.com>
2021-09-04 16:45:20 +0900

    [APIGATEWAY] 첫번째 커밋 간단한 필터 작업중

---
+++ MAKDUCK-apigateway-service/src/main/java/com/suleesulee/MAKDUCKapigatewayservice/MakduckApigatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.suleesulee.MAKDUCKapigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakduckApigatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakduckApigatewayServiceApplication.class, args);
+	}
+
+}
---
+++ MAKDUCK-apigateway-service/src/test/java/com/suleesulee/MAKDUCKapigatewayservice/MakduckApigatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suleesulee.MAKDUCKapigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakduckApigatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
