aca1ee3257058724f016750758fae84f5c06ef76
suleesulee <56288518+suleesulee@users.noreply.github.com>
2021-09-14 16:23:46 +0900

    feat: api-gateway에 웹페이지가 포함되어 web 의존성을 추가, bootstrap을 적용하여 index.html 추가

--- MAKDUCK-apigateway-service/src/main/java/com/suleesulee/MAKDUCKapigatewayservice/MakduckApigatewayServiceApplication.java
+++ MAKDUCK-apigateway-service/src/main/java/com/suleesulee/MAKDUCKapigatewayservice/MakduckApigatewayServiceApplication.java
@@ -1,8 +1,11 @@
 package com.suleesulee.MAKDUCKapigatewayservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
+@Slf4j
 @SpringBootApplication
 public class MakduckApigatewayServiceApplication {
 
